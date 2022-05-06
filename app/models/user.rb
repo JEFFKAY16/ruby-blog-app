5f8c1d746480beb740307d3d78a39df245a77cfa
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-05-06 12:29:59 +0200

    Add data processing to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_many :posts, class_name: 'Post'
+  has_many :comments, class_name: 'Like'
+  has_many :likes, class_name: 'Comment'
+
+  def most_recent_posts
+    posts.limit(3).order(created_at: :desc)
+  end
+
+  # has_many :posts, foreign_key: 'author_id', class_name: 'Post'
+  # has_many :comments, foreign_key: 'author_id', class_name: 'Like'
+  # has_many :likes, foreign_key: 'author_id', class_name: 'Comment'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author: first_user, title: 'Hello', text: 'This is my second post')
+third_post = Post.create(author: first_user, title: 'Hello', text: 'This is my third post')
+fourth_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+fith_post = Post.create(author: first_user, title: 'Hello', text: 'This is my fith post')
+
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
