474fde0c21257a84e88f1ecb64d3b2dab6003f3d
Jeff Kayombo <jeffkayombo16@gmail.com>
2022-05-12 21:09:27 +0200

    Fix linters

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  
   describe 'The comments model' do
     before(:each) do
       first_user = User.create!(name: 'Jeff', photo: 'profile.jpg', bio: 'We love th earth, it is our planet')
 
-      first_post = Post.create!(author_id: first_user.id, title: 'Earth', text: 'We love th earth, it is our planet by lil Dicky')
+      first_post = Post.create!(author_id: first_user.id, title: 'Earth',
+                                text: 'We love th earth, it is our planet by lil Dicky')
 
       Comment.create!(post_id: first_post.id, author_id: first_user.id, text: 'Living Earth')
     end
@@ -19,5 +19,4 @@ RSpec.describe Comment, type: :model do
       expect(Comment.all.length).to eq 1
     end
   end
-
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  
   describe 'The post model' do
     before(:each) do
       first_user = User.create!(name: 'Jeff', photo: 'profile.jpg', bio: 'Text user yoh.')
-      Post.create!(author_id: first_user.id, title: 'Enviro Rehab', text: 'Gotta love the environemnt and care for it yeah')
+      Post.create!(author_id: first_user.id, title: 'Enviro Rehab',
+                   text: 'Gotta love the environemnt and care for it yeah')
     end
 
     it 'should not have a blank title' do
@@ -20,11 +20,15 @@ RSpec.describe Post, type: :model do
       expect(Post.first.text).to eq('Gotta love the environemnt and care for it yeah')
     end
 
+    # rubocop:disable Layout/LineLength
+
     it 'checks character count' do
       post = Post.first
       post.title = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ultrices dui eu tempor  xfsdgff sgfg frfrgfe suscipit. Ut sagittis risus nisi, fermentum ultricies arcu interdum et. Aenean interdum pretium magna. Etiam efficitur aliquam vestibulum. Duis convallis odio nisi. Cras sagittis sagittis turpis, non tincidunt augue sagittis at. Vivamus nec dui et lectus venenatis porttitor. Sed ut sagittis leo, egestas porttitor.'
       expect(post).to_not be_valid
     end
+
+    # rubocop:enable Layout/LineLength
 
     it 'checks likes counter' do
       expect(Post.first.comments_counter).to eq(0)
@@ -41,5 +45,4 @@ RSpec.describe Post, type: :model do
       expect(Post.first.comments_counter).to be_a(Integer)
     end
   end
-
 end
